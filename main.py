91610608aa80265be9f97be92bf4b499e2959bd9
Ieva Hermane <ieva.hermane@s1vsk.lv>
2021-01-07 08:52:59 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+#Komentārus var rakstīt
+#vairākās
+#rindās"""
+print("Sveika, pasaule!")
+#print(2 * 2)
+print(2 * 2, 2 * 3, 2 * 4, "Pitons")
+print(f"Ja saskaitīsim 5 ar 7, iegūsim {5+7}.")
+print("Sveika, "+"pasaule!") #var rakstīt komentārus
+print("Ieva "*7)
+print(60*60*24*365)
+print(0.1+0.2-0.3)
