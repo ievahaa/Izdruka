92128ff22d51c3d5cdea42ef6620629ff89efb35
Ieva Hermane <ieva.hermane@s1vsk.lv>
2021-01-07 10:39:05 +0000

    Dzēsts komentārs

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-#Komentārus var rakstīt
-#vairākās
-#rindās"""
 print("Sveika, pasaule!")
-#print(2 * 2)
+print(2 * 2)
 print(2 * 2, 2 * 3, 2 * 4, "Pitons")
-print(f"Ja saskaitīsim 5 ar 7, iegūsim {5+7}.")
-print("Sveika, "+"pasaule!") #var rakstīt komentārus
-print("Ieva "*7)
-print(60*60*24*365)
-print(0.1+0.2-0.3)
-print("Tūliņ beigsies stunda")
+print(f"Ja saskaitīsim 5 ar 7, iegūsim {5+7}."
+      )  #kombinētā izdruka - teksts un aprēķins
+print("Sveika, " + "pasaule!")
+#Izdrukājiet savu vārdu 5 reizes
+print("Ieva " * 5)
+print(365 * 60 * 60 * 24)
+print(0.1 + 0.2 - 0.3)
