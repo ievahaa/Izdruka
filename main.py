8c9cd482efd5622e5a4818bc2eaf7b95467a7f29
Ieva Hermane <ieva.hermane@s1vsk.lv>
2021-01-11 13:21:29 +0000

    Datu ievadei

--- main.py
+++ main.py
@@ -10,3 +10,22 @@ print(365 * 60 * 60 * 24)
 print(0.1 + 0.2 - 0.3)
 print()
 
+#Mainīgie
+a = 5
+print(a)
+print(a + a)
+a = a + a + a #Ar = piešķir vērtību
+print(a)
+print(type(a))
+a = 30.1
+print(type(a))
+
+#Nodokļi
+mani_ienakumi = 367
+nodoklis = 0.23 #10%
+maniNodokli = mani_ienakumi * nodoklis
+print(maniNodokli)
+
+#Datu ievade
+x = input("Ievadi vārdu: ")
+print("Tavs ievadītais vārds ir " + x)
