47fa8b95d6bc2edbb6d97543592b54d1ec7efb4a
Ieva Hermane <>
2021-04-12 13:35:21 +0000

    papildināti uzdevumu risinājumi

--- funkc_uzd.py
+++ funkc_uzd.py
@@ -28,4 +28,31 @@ print(ir_problema(False, False))
 print(ir_problema(True, True))
 print(ir_problema(False, True))
 
-#Dots veselais skaitlis n. Uzraksti funkciju, kas atgriež starpības moduli starp n un 21, izņemot, ja modulis ir vairāk nekā 21, tad atgriež dubultu tā vērtību. Ja n>21, tad starpība n-21 ir pozitīva, ja n<21, tad starpība 21-n ir pozitīva
+#Dots pozitīvs veselais skaitlis n. Uzraksti funkciju, kas atgriež starpības moduli starp n un 21, izņemot, ja modulis ir vairāk nekā 21, tad atgriež dubultu tā vērtību.
+
+
+def starpiba(n):
+    if n <= 21:
+        st = 21 - n
+    else:
+        st = n - 21
+    if st > 21:
+        return st * 2
+    return st
+
+
+print(starpiba(3)) #atgriež 18, jo 21-3 = 18
+print(starpiba(121)) #atgriež 200, jo 121-21 =100, bet jāreizina ar 2
+
+#We have a loud talking parrot. The "hour" parameter is the current hour time in the range 0..23. We are in trouble if the parrot is talking and the hour is before 7 or after 20. Return True if we are in trouble.
+
+
+def papagaila_problema(laiks, runa):
+    if (laiks < 7 or laiks > 20) and runa:
+        return True
+    return False
+
+
+print(papagaila_problema(5, True))  #atgriež True, jo trokšņo naktī
+print(papagaila_problema(15, True))  #atgriež False, jo trokšņo dienā
+print(papagaila_problema(5, False))  #atgriež False, jo naktī netrokšņo
